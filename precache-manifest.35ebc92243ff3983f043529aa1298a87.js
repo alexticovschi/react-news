6dbb03904654851847acd42672ef2bc523c023ef
AlexTicovschi <alexmticovschi@gmail.com>
2019-01-05 16:48:58 +0000

    Updates

--- precache-manifest.35ebc92243ff3983f043529aa1298a87.js
+++ precache-manifest.35ebc92243ff3983f043529aa1298a87.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/react-news/static/js/runtime~main.d652ad56.js"
   },
   {
-    "revision": "12589870743e84878b17",
-    "url": "/react-news/static/js/main.12589870.chunk.js"
+    "revision": "278e9b5a19caf5050e00",
+    "url": "/react-news/static/js/main.278e9b5a.chunk.js"
   },
   {
     "revision": "deb51cddaaa0bd26be5d",
     "url": "/react-news/static/js/1.deb51cdd.chunk.js"
   },
   {
-    "revision": "12589870743e84878b17",
+    "revision": "278e9b5a19caf5050e00",
     "url": "/react-news/static/css/main.8a9d5070.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/react-news/static/css/1.3657dac3.chunk.css"
   },
   {
-    "revision": "1038d7fe2bd794743e969f6836e9854e",
+    "revision": "52839690d7a2fa1516839c6d5178ed12",
     "url": "/react-news/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-news/precache-manifest.4bfaf6ff68caeafba800f6abe434068e.js"
+  "/react-news/precache-manifest.35ebc92243ff3983f043529aa1298a87.js"
 );
 
 workbox.clientsClaim();
